0ae62b26381cf9db7f0895564a5c7954a3ce1c7f
Cay <caleb.wilkerson@sacawarriors.org>
2026-01-29 10:26:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ print(Welcome to Caleb's quiz)
 
 print("question: what is the first book in the bible?")
 
-if answer == "Genesis":
+if 'answer' == "Genesis":
  print("correct!")
+
+elif 'answer' == "genesis":
+ print("correct!")
+
+else 'answer' == "1st in the bible":
+ print(wrong)
   
